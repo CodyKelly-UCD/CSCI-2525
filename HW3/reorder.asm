a3b9ed4e491ec1fbaa57c298fc29c6b22a6334c9
Cody Kelly <codykelly@Codys-iMac.local>
2018-02-17 13:14:39 -0700

    Added homework 3 and updated gitignore

---
+++ HW3/reorder.asm
@@ -0,0 +1,18 @@
+TITLE reorder.asm
+; Rearranges the values of an array into a different order
+; Uses only: 
+; MOV and XCHG
+; Direct offset addressing
+
+INCLUDE Irvine.32
+
+.data
+
+.data?
+
+.code
+main PROC
+
+exit
+main ENDP
+END main
