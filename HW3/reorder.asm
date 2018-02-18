2cd314956404b1f24422acc4484ca9588c2a35a2
Cody Kelly <cody.kelly@ucdenver.edu>
2018-02-17 21:32:59 -0700

    Finished part 1 of HW3

--- HW3/reorder.asm
+++ HW3/reorder.asm
@@ -4,14 +4,23 @@ TITLE reorder.asm
 ; MOV and XCHG
 ; Direct offset addressing
 
-INCLUDE Irvine.32
+INCLUDE Irvine32.inc
 
 .data
-
+clearEAX TEXTEQU <mov eax, 0>
+clearEBX TEXTEQU <mov ebx, 0>
+arrayD DWORD 32, 51, 12
 .data?
 
 .code
 main PROC
+
+clearEAX
+clearEBX
+MOV eax, [arrayD]		; Move 32 to EAX
+XCHG eax, [arrayD + 4]	; Exchange 32 and 51
+XCHG eax, [arrayD + 8]	; Exchange 51 and 12
+MOV [arrayD], eax		; Move 12 to first position
 
 exit
 main ENDP
