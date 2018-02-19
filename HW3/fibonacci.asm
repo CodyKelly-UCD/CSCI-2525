d6cdc11cb381ec88e82c4f26598b90ce4da4cafc
Cody Kelly <cody.kelly@ucdenver.edu>
2018-02-18 19:33:13 -0700

    Working on HW3

--- HW3/fibonacci.asm
+++ HW3/fibonacci.asm
@@ -10,11 +10,21 @@ TITLE fibonacci.asm
 INCLUDE Irvine32.inc
 
 .data
-
+clearECX TEXTEQU <mov ecx, 0>
+clearEAX TEXTEQU <mov eax, 0>
+fibArray BYTE 10 DUP(0)
 .data?
 
 .code
 main PROC
+mov esi, OFFSET fibArray
+MOV [esi], 1			    ; Set the second element in fibArray equal to fib(1)
+MOV ecx, LENGTHOF fibArray - 2	    ; Set the loop counter to length of fibArray minus 
+                                    ; two because we know the values of the first two elements
+NEG ecx
+fib:
+    MOV [esi + ecx], 1
+    LOOP fib
 
 exit
 main ENDP
