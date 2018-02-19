b01546a0d9dc7d25d88fe145cca43efae8dff9b2
unknown <cody.kelly@ucdenver.edu>
2018-02-19 13:09:28 -0700

    Restored HW3/reorder.asm to original

--- HW3/reorder.asm
+++ HW3/reorder.asm
@@ -9,7 +9,7 @@ INCLUDE Irvine32.inc
 .data
 clearEAX TEXTEQU <mov eax, 0>
 clearEBX TEXTEQU <mov ebx, 0>
-arrayD BYTE 32, 51, 12
+arrayD DWORD 32, 51, 12
 .data?
 
 .code
@@ -17,10 +17,10 @@ main PROC
 
 clearEAX
 clearEBX
-MOV al, [arrayD]       ; Move 32 to EAX
-XCHG al, [arrayD + 4]  ; Exchange 32 and 51
-XCHG al, [arrayD + 8]  ; Exchange 51 and 12
-MOV [arrayD], al       ; Move 12 to first position
+MOV eax, [arrayD]       ; Move 32 to EAX
+XCHG eax, [arrayD + 4]  ; Exchange 32 and 51
+XCHG eax, [arrayD + 8]  ; Exchange 51 and 12
+MOV [arrayD], eax       ; Move 12 to first position
 
 exit
 main ENDP
