c3602a1411324779606ee46b7ee0cd8f86daca0a
unknown <cody.kelly@ucdenver.edu>
2018-02-18 17:04:34 -0700

    Completed HW3

--- HW3/fibonacci.asm
+++ HW3/fibonacci.asm
@@ -1,30 +1,43 @@
 TITLE fibonacci.asm
-; Completes the following:
-;   a. fib(n) for n = 2, 3, ..., 10 using an array of the appropriate 
-;      size and type. Declares a value for fib(0) and fib(1), but all
-;      remaining elements are calculated by the program.
-;   b. After the array is filled with required values, the program 
-;      stores fib(3) through fib(6) in consecutive bytes of the ebx
-;      register starting from the lowest byte
-
+COMMENT !
+ Completes the following:
+   a. fib(n) for n = 2, 3, ..., 10 using an array of the appropriate 
+      size and type. Declares a value for fib(0) and fib(1), but all
+      remaining elements are calculated by the program.
+   b. After the array is filled with required values, the program 
+      stores fib(3) through fib(6) in consecutive bytes of the ebx
+      register starting from the lowest byte
+!
 INCLUDE Irvine32.inc
 
 .data
-clearECX TEXTEQU <mov ecx, 0>
-clearEAX TEXTEQU <mov eax, 0>
-fibArray BYTE 10 DUP(0)
+clearECX	 TEXTEQU	 <mov ecx, 0>
+clearEAX	 TEXTEQU	 <mov eax, 0>
+fibArray	 BYTE 10 DUP(0)
 .data?
 
 .code
 main PROC
-mov esi, OFFSET fibArray
-MOV [esi], 1			    ; Set the second element in fibArray equal to fib(1)
-MOV ecx, LENGTHOF fibArray - 2	    ; Set the loop counter to length of fibArray minus 
+clearECX
+clearEAX
+mov [fibArray], 0                   ; Set first element to fib(0)
+mov [fibArray + 1], 1			    ; Set the second element equal to fib(1)
+mov ecx, LENGTHOF fibArray - 2	    ; Set the loop counter to length of fibArray minus 
                                     ; two because we know the values of the first two elements
-NEG ecx
-fib:
-    MOV [esi + ecx], 1
-    LOOP fib
+mov esi, OFFSET fibArray
+
+fibLoop:
+    mov al, [esi]                   ; al = fib(n - 2)
+    inc esi
+    mov ah, [esi]                   ; ah = fib(n - 1)
+    inc esi
+    mov [esi], al
+    add [esi], ah                   ; fibArray(n) = fib(n-2) + fib(n-1)
+    dec esi
+    loop fibLoop
+
+mov ebx, DWORD PTR fibArray + 3     ; set ebx equal to fib(3) through fib(6)
+call DumpRegs
 
 exit
 main ENDP
